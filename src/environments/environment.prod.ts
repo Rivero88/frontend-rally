4a37b44bb491bcfe0c6b84f80cba71f51dbd5dc3
Rivero88 <127329709+Rivero88@users.noreply.github.com>
2025-05-22 16:51:02 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://backend-rally-production-fb67.up.railway.app'
+  API_URL: 'https://backend-rally-production-3d49.up.railway.app'
 };
